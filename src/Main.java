8255b1449a23ca1f40002d148451c329ad6d90fe
TatianaShV <artemis1192@mail.ru>
2023-01-16 22:04:36 +0300

    Добавлен int average

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-SalesManager salesManager = new SalesManager(new int[] {60, 9, 100, 90});
-int max = salesManager.max();
+        SalesManager salesManager = new SalesManager(new int[]{60, 9, 100, 90});
+        int max = salesManager.max();
         System.out.println(max);
-
+        int min = salesManager.min();
+        int average = salesManager.average();
+        System.out.println(min);
+        System.out.println(average);
     }
 }
